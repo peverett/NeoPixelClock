e037f1474b6fb76c37f5ba22fd2288adce27223f
peverett <simon.peverett@gmail.com>
2017-04-16 15:34:16 +0100

    Time display functions changed to classes.

--- NeopixelClock.ino
+++ NeopixelClock.ino
@@ -59,6 +59,10 @@ Adafruit_NeoPixel ring60 = Adafruit_NeoPixel(RING60_MAX, RING60_PIN, NEO_GRB + N
 Adafruit_NeoPixel ring24 = Adafruit_NeoPixel(RING24_MAX, RING24_PIN, NEO_GRB + NEO_KHZ800);
 SSD1306AsciiWire oled;
 
+int disp_mode;
+
+DateTime now;
+DateTime then;
 
 // Function pointer typedef
 typedef void (*DISP_FUNC_T)(DateTime now, DateTime then, bool init);
@@ -211,86 +215,104 @@ char oled_mode_names[][11] = {
   "Date      "
  };
 
-// ArcTime
-static void ArcTime(DateTime now, DateTime then, bool init=false) {
-  int pixel;
 
-  if (init) clear_rings();
-
-  if (init || ( now.minute() != then.minute() ) ) {
-    pixel = now.minute();
-    if (pixel == 0) {
-      for (int idx=0; idx<RING60_MAX; idx++) ring60.setPixelColor(idx, 0, 0 ,0);
-    }
-    else {
-      for (int idx=0; idx<=pixel; idx++) {
-        ring60.setPixelColor( idx, clr[0], clr[1], clr[2]);
-        next_color();
-      }
-    }    
-    ring60.show();
-  }
-
-  if (init || ( now.hour() != then.hour() ) ) {
-    if (now.hour() == 0 || now.hour() == 12) {
-      for (int idx=0; idx<RING24_MAX; idx++) ring24.setPixelColor(idx, 0, 0, 0);
-      ring24.setPixelColor(0, clr[0], clr[1], clr[2]);
-    }
-    else {
-      pixel = ( (now.hour() >= 12) ? now.hour()-12 : now.hour() ) * 2;
-      for (int idx=0; idx<=pixel; idx++) {
-        ring24.setPixelColor( idx, clr[0], clr[1], clr[2]);
-        next_color();
-      }
-    }
-    ring24.show();
-  }
+/*!
+ * @brief convert 24-hour clock time to 12-hour.
+ * 
+ * @param[in] twenty_four Twenty four hour clock time, range 0..23.
+ */
+inline uint8_t TwentyFourToTwelve(uint8_t twenty_four) {
+  return (twenty_four >= 12) ? twenty_four - 12 : twenty_four;
 }
 
-static void RingTime(DateTime now, DateTime then, bool init=false) {
-  if (init) clear_rings();
-  
-  if ( init || 
-      (now.second() != then.second()) || 
-      (now.minute() != then.minute()) ) {
+/*!
+ * @brief Neopixel Display Base Class
+ * 
+ * In the HW there are two NeoPixel rings
+ * - One ring with 24-NeoPixel - used for hours.
+ * - One ring with 60-NeoPixel - used for minutes and seconds.
+ */
+class BaseTimeDisplay {
+public:
+  BaseTimeDisplay(Adafruit_NeoPixel &r24, Adafruit_NeoPixel &r60) :
+    ring24( r24 ),
+    ring60( r60 )
+    {};
+
+  /*!
+   * @brief Display the current time passed in 'tn', intialising the display
+   * 
+   * Initialises the display for time now 'tn', setting all NeoPixels. This
+   * is a virtual function in the base class and must be overridden in 
+   * the derived classes/
+   * 
+   * @param[in] tn Time now in DateTime format.
+   */
+  virtual void Display(const DateTime tn) {};
+
+  /*!
+   * @brief Update the display for the difference between 'tn' - time now - and
+   * 'tt' - time then. 
+   * 
+   * The point of using Update is that it should only change as much of the 
+   * Neopixel display as is required to show the new time, without setting
+   * all the Neopixels. For example. If only minutes have changed, only update
+   * the 60-NeoPixel ring, don't change the 24-NeoPixel ring.
+   * 
+   * @param[in] tn Time now in DateTime format.
+   * @param[in] tt Time then in DateTime format.
+   */
+   virtual void Update(const DateTime tn, const DateTime tt) {};
+      
+protected:
+  Adafruit_NeoPixel ring24;
+  Adafruit_NeoPixel ring60;
+};
+
+/*!
+ * @brief Display time as growing arcs.
+ * 
+ * - Hours are displayed as an Arc starting from the 12 o'clock postion.
+ * - Likewise minutes, and seconds. 
+ * - The colour of minutes and seconds are combined so that when the arcs overlap
+ *   the NeoPixel color is that of both seconds and minutes.
+ * - The colours change depending on whether the time is before or after mid-day.
+ */
+class ArcTimeDisplay: public BaseTimeDisplay {
+public:
+  ArcTimeDisplay(Adafruit_NeoPixel &r24, Adafruit_NeoPixel &r60)
+    : BaseTimeDisplay(r24, r60)
+    {};
+
+  void Display(const DateTime tn) {
+    uint8_t hr = TwentyFourToTwelve(tn.hour()) * 2;
+
     int red, green, blue;
 
     for (int idx=0; idx<RING60_MAX; idx++) {
-      if (now.hour() < 12) { 
-        blue = (idx <= now.second()) ? 30 : 0;
+      if (tn.hour() < 12) { 
+        blue = (idx <= tn.second()) ? 30 : 0;
         red = 0;
       }
       else {
         blue = 0;
-        red = (idx <= now.second()) ? 30: 0;
+        red = (idx <= tn.second()) ? 30: 0;
       }
-      green = (idx <= now.minute()) ? 30 : 0;
-      ring60.setPixelColor( idx, red, green, blue );
+      green = (idx <= tn.minute()) ? 30 : 0;
+      ring60.setPixelColor(idx, red, green, blue );
     }
-    ring60.show();
-  }
 
-  if (init | (now.hour() != then.hour()) ) {
-    int red, blue;
-    int hr = ( (now.hour() >= 12) ? now.hour() - 12 : now.hour() ) * 2;
-    
-    if (now.hour() < 12) {
-      red = 30;
-      blue = 0;
-    } 
-    else {
-      red = 0;
-      blue = 30;
-    }
+    red = (tn.hour() < 12) ? 30 : 0;
+    blue = (tn.hour() < 12) ? 0: 30;
     
     /* At mid-night no hours are shown. */
-    if (now.hour() == 0) {
+    if (tn.hour() == 0) {
       for (int idx=0; idx<=RING24_MAX; idx++) {
         ring24.setPixelColor(idx, 0, 0, 0); 
       }     
     }
     /* At mid-day the full ring is shown in blue. */
-    else if (now.hour() == 12) {
+    else if (tn.hour() == 12) {
       for (int idx=0; idx<=RING24_MAX; idx++) {
         ring24.setPixelColor(idx, red, 0, blue);
       }
@@ -304,57 +326,230 @@ static void RingTime(DateTime now, DateTime then, bool init=false) {
       }
     }
     ring24.show();
-  }
-    
-}
-
-static void ShowTime(DateTime now, DateTime then, bool init=false) {
-
-  int hr;
-  int pix_hour;
-
-  if (init) clear_rings();
-
-  if ( init || 
-      (now.second() != then.second()) ||
-      (now.minute() != then.minute()) ) {
-    for (int idx=0; idx<RING60_MAX; idx++) {
-      if ( (idx % 5) == 0 )
-        ring60.setPixelColor( idx, 10, 10, 10 );
-      else
-        ring60.setPixelColor( idx, 0, 0 , 0 );
-    }
-//    ring60.setPixelColor( pix_hour, 30, 0, 0 );
-    ring60.setPixelColor( now.minute(), 0, 30, 0 );
-    ring60.setPixelColor( now.second(), 0, 0, 30 );
-  
     ring60.show();
-  }
+  };
 
-  if (init || (now.hour() != then.hour() ) ) {
-    for (int idx=0; idx<RING24_MAX; idx++) {
-      ring24.setPixelColor( idx, 0, 0, 0 );
+  /* Only updates seconds and minutes. On minute transition, just redraw the whole 
+   * display.
+   */
+  void Update(const DateTime tn, const DateTime tt) {
+    if (tn.minute() != tt.minute()) {
+      Display(tn);
+    } 
+    else if (tn.second() != tt.second()) {
+      int red, green, blue;
+
+      blue = (tn.hour() < 12) ? 30 : 0;
+      red = (tn.hour()< 12) ? 0 : 30;
+      green = (tn.second() <= tn.minute()) ? 30 : 0;
+
+      ring60.setPixelColor(tn.second(), red, green, blue);
+      ring60.show();
     }
-  
-    hr = (now.hour() >= 12) ? now.hour()-12 : now.hour();
-    pix_hour = hr * 2;
-    ring24.setPixelColor(pix_hour, 30, 0, 0);
-    ring24.show();
-  }
-}
-
-DISP_FUNC_T disp_func[] = { ShowTime, ArcTime, RingTime };
-const int disp_func_max = (int)sizeof(disp_func)/sizeof(DISP_FUNC_T);
-char disp_func_names[][11] = {
-  "Default   ",
-  "Pastel Arc",
-  "RGB Ring  "
+  };
 };
 
-int disp_mode;
 
-DateTime now;
-DateTime then;
+/*!
+ * @brief Displays pastel rings of NeoPixels with the hours and minutes 
+ *        indicated by blinking the hour and minute NeoPixels.
+ *        
+ * - Both 60 and 24-Neopixel ring are illuminated except for time indicating
+ *   NeoPixel, which blinks.
+ * - The colors on both rings rotatate fully, once per minute (approx).
+ */
+class PastelTimeDisplay: public BaseTimeDisplay {
+public:
+  PastelTimeDisplay(Adafruit_NeoPixel &r24, Adafruit_NeoPixel &r60)
+    : BaseTimeDisplay(r24, r60)
+    {};
+
+  void Display(const DateTime tn) {
+    int clr[3] = {20, 0, 0};
+    int sub_idx = 0;
+    int add_idx = 1;
+    int step_val = 2;
+    uint8_t hr = TwentyFourToTwelve(tn.hour()) * 2;
+
+    for (int idx=0; idx < RING60_MAX; idx++) {
+      ring60.setPixelColor(idx, clr[0], clr[1], clr[2]);
+      if (clr[add_idx] == 20) {
+        sub_idx = add_idx;
+        add_idx = (add_idx+1) % 3;      
+      }
+      clr[add_idx]++;
+      clr[sub_idx]--;      
+    }
+
+    for (int idx=0; idx<RING24_MAX; idx++) {
+      ring24.setPixelColor(idx, clr[0], clr[1], clr[2]);
+      if (clr[add_idx] == 20) {
+        sub_idx = add_idx;
+        add_idx = (add_idx+1) % 3;      
+      }
+      clr[add_idx]+=step_val;
+      clr[sub_idx]-=step_val;
+      step_val = (step_val == 2) ? 3 : 2;
+    }
+
+    hour_clr = ring24.getPixelColor(hr);
+    minute_clr = ring60.getPixelColor(tn.minute());
+
+    ring24.setPixelColor(hr, 0);
+    ring60.setPixelColor(tn.minute(), 0);
+
+    ring60.show();
+    ring24.show();
+
+    minute_tmr = millis() + mn_delay;
+    hour_tmr = millis() + hr_delay;
+    blink_tmr = millis() + bk_delay;
+    led_off = true;
+  };
+
+  void Update(const DateTime tn, const DateTime tt) {
+    int tn_hr = TwentyFourToTwelve(tn.hour()) * 2;
+    int tt_hr = TwentyFourToTwelve(tt.hour()) * 2;
+    uint32_t temp;
+
+    ring24.setPixelColor(tt_hr, hour_clr);
+    ring60.setPixelColor(tt.minute(), minute_clr);
+
+    if (millis() > minute_tmr) {
+        temp = ring60.getPixelColor(RING60_MAX-1);
+        for (int idx=RING60_MAX-1; idx > 0; idx--) {
+        ring60.setPixelColor(idx, ring60.getPixelColor(idx-1));
+      }
+      ring60.setPixelColor(0, temp); 
+      minute_tmr = millis() + mn_delay;
+    }
+
+    if (millis() > hour_tmr) {
+      temp = ring24.getPixelColor(RING24_MAX-1);
+      for (int idx=RING24_MAX-1; idx > 0; idx--) {
+        ring24.setPixelColor(idx, ring24.getPixelColor(idx-1));
+      }
+      ring24.setPixelColor(0, temp);
+      hour_tmr = millis() + hr_delay;
+    }
+
+    hour_clr = ring24.getPixelColor(tn_hr);
+    minute_clr = ring60.getPixelColor(tn.minute());
+    if (led_off) {
+        ring24.setPixelColor(tn_hr, 0);
+        ring60.setPixelColor(tn.minute(), 0);
+    }
+    if (millis() > blink_tmr) {
+      led_off = (led_off) ? false : true;
+      blink_tmr = millis() + bk_delay;
+    }
+
+    ring24.show();
+    ring60.show();
+  };
+
+private:
+  uint32_t minute_clr;
+  uint32_t hour_clr;
+  unsigned long minute_tmr;
+  unsigned long hour_tmr;
+  unsigned long blink_tmr;
+  bool  led_off;
+
+  const int mn_delay = 1000;
+  const int hr_delay = 2500;
+  const int bk_delay = 500;
+};
+
+
+/*!
+ * @brief Simulates an analogue clock desplay
+ * 
+ * - The 'hour' positions are highlighted by illuminating them white.
+ * - The hours are shown on the 24-NeoPixel ring as red.
+ * - Minutes are shown on the 60-NeoPixel ring as green.
+ * - Seconds are shown on the 60-NeoPixel ring as blue.
+ * 
+ * Minutes occlude the hour markers, and seconds occlude both of those.
+ */
+class AnalogTimeDisplay: public BaseTimeDisplay {
+public:
+  AnalogTimeDisplay(Adafruit_NeoPixel &r24, Adafruit_NeoPixel &r60)
+    : BaseTimeDisplay(r24, r60)
+    {};
+
+  void Display(const DateTime tn) {
+    uint8_t hr = TwentyFourToTwelve(tn.hour()) * 2;
+
+    for (int idx=0; idx < RING60_MAX; idx++) {
+      if ((idx % 5) == 0) 
+        ring60.setPixelColor(idx, 10, 10, 10);
+      else
+        ring60.setPixelColor(idx, 0, 0, 0);
+    }
+    ring60.setPixelColor(tn.minute(), 0, 30, 0);
+    ring60.setPixelColor(tn.second(), 0, 0, 30);
+
+    for (int idx=0; idx<RING24_MAX; idx++) {
+      ring24.setPixelColor(idx, 0, 0, 0);
+    }
+    ring24.setPixelColor(hr, 30, 0, 0);
+
+    ring60.show();
+    ring24.show();
+  };
+
+  void Update(const DateTime tn, const DateTime tt) {
+    int tn_hr = TwentyFourToTwelve(tn.hour()) * 2;
+    int tt_hr = TwentyFourToTwelve(tt.hour()) * 2;
+    bool show60 = false;
+    
+    if (tn_hr != tt_hr) {
+      ring24.setPixelColor(tn_hr, 30, 0, 0);
+      ring24.setPixelColor(tt_hr, 0, 0, 0);
+      ring24.show();
+    }
+
+    if (tn.minute() != tt.minute()) {
+      if ((tt.minute() % 5) == 0)
+        ring60.setPixelColor(tt.minute(), 10, 10, 10);
+      else
+        ring60.setPixelColor(tt.minute(), 0, 0, 0);
+
+      ring60.setPixelColor(tn.minute(), 0, 30, 0);
+      show60=true;
+    }
+    
+    if (tn.second() != tt.second()) {
+      if (tt.second() == tn.minute())
+        ring60.setPixelColor(tn.minute(), 0, 30, 0);
+      else if ((tt.second() % 5) == 0)
+        ring60.setPixelColor(tt.second(), 10, 10, 10);
+      else
+        ring60.setPixelColor(tt.second(), 0, 0, 0);
+
+      ring60.setPixelColor(tn.second(), 0, 0, 30);
+      show60=true;
+    }
+
+    if (show60) ring60.show();
+  };
+};
+
+AnalogTimeDisplay analog_time(ring24, ring60);
+ArcTimeDisplay    arc_time(ring24, ring60);
+PastelTimeDisplay pastel_time(ring24, ring60);
+
+BaseTimeDisplay *time_display[] = { &analog_time, &arc_time, &pastel_time };
+
+const int disp_mode_max = sizeof(time_display) / sizeof(BaseTimeDisplay*);
+
+char disp_mode_names[][11] = {
+  "Analog   ",
+  "RGB Arc  ",
+  "Pastel   "
+};
+
 
 /*!
  * @brief Configure the Neopixel display mode of the clock.
@@ -373,9 +568,8 @@ bool configure_display() {
   while(true) {
     red_button_press = false;
     oled.setCursor(0, oled.fontRows());
-    oled.print(disp_func_names[disp_mode]);
+    oled.print(disp_mode_names[disp_mode]);
     oled.clearToEOL();
-    disp_func[disp_mode](now, then, true);
 
     while(!red_button_press) {
       if (digitalRead(BLU_BTTN_PIN) == LOW) {
@@ -383,10 +577,11 @@ bool configure_display() {
       }
       else if (digitalRead(RED_BTTN_PIN) == LOW) {
         red_button_press = long_button_press(RED_BTTN_PIN, 10);
-        disp_mode = (disp_mode + 1) % disp_func_max;
+        disp_mode = (disp_mode + 1) % disp_mode_max;
+        time_display[disp_mode]->Display(now);
       }
       now = rtc.now();
-      disp_func[disp_mode](now, then, false);
+      time_display[disp_mode]->Update(now, then);
       then = now;
       delay(100);    
     }
@@ -427,7 +622,7 @@ bool configure_oled() {
         oled_mode = (oled_mode + 1) % oled_mode_max;
       }
       now = rtc.now();
-      disp_func[disp_mode](now, then, false);
+      time_display[disp_mode]->Update(now, then);
       then = now;
       delay(100);    
     }
@@ -450,7 +645,6 @@ bool configure_time_hour() {
   oled.println("Set Hours");
 
   now = rtc.now();
-  disp_func[disp_mode](now, then, true);
   while(true) {
     red_button_press = false;
     oled.setCursor(0, oled.fontRows());
@@ -474,7 +668,7 @@ bool configure_time_hour() {
             0
           );
       }
-      disp_func[disp_mode](now, then, false);
+      time_display[disp_mode]->Update(now, then);
       then = now;
       delay(100);    
     }
@@ -497,11 +691,11 @@ bool configure_time_minute() {
   oled.println("Set Minute");
 
   now = rtc.now();
-  disp_func[disp_mode](now, then, true);
   while(true) {
     oled.setCursor(0, oled.fontRows());
     oled.print(now.minute());
-    
+    time_display[disp_mode]->Display(now);
+
     oled.clearToEOL();
       if (digitalRead(BLU_BTTN_PIN) == LOW) {
         rtc.adjust(now);
@@ -519,8 +713,9 @@ bool configure_time_minute() {
             minute, 
             0
           );
+        time_display[disp_mode]->Display(now);
       }
-      disp_func[disp_mode](now, then, false);
+      // time_display[disp_mode]->Update(now, then);
       delay(100);    
   }
 }
@@ -546,8 +741,8 @@ void configure() {
   if (configure_oled()) return;
   if (configure_time_hour()) return;
   if (configure_time_minute()) return;
+  then = now;
 }
-
 
 void setup() {
   PRINT_INIT( 9600 );
@@ -574,7 +769,7 @@ void setup() {
   oled.clear();
 
   now = then = rtc.now();
-  disp_func[disp_mode](now, then, true);
+  time_display[disp_mode]->Display(now);
   oled_func[oled_mode](now, then, true);
 
   pinMode(RED_BTTN_PIN, INPUT_PULLUP);
@@ -591,7 +786,7 @@ void loop() {
     }
         
     now = rtc.now();
-    disp_func[disp_mode](now, then, false);
+    time_display[disp_mode]->Update(now, then);
     oled_func[oled_mode](now, then, false);
     then = now;    
 }
